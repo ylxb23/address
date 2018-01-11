83dcdc80e4de52054747b15b4e6dea16365901cc
ylxb23 <ylxb23@163.com>
2018-01-11 21:54:57 +0800

    补充数据

--- sql/area.sql
+++ sql/area.sql
@@ -2,9 +2,9 @@ CREATE TABLE `area` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
   `area_code` varchar(15) NOT NULL COMMENT '地区编码',
   `name` varchar(45) NOT NULL COMMENT '地区名称',
-  `alia_name` varchar(45) NOT NULL COMMENT '地区别名',
+  `alia_name` varchar(200) NOT NULL COMMENT '地区别名',
   `parent_code` varchar(8) NOT NULL COMMENT '父级地区编码',
   `level` tinyint(4) NOT NULL COMMENT '地区级别(大陆:1-省,2-市,3-区/县)',
   `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '类型(0-一般,1-双地名,2-双地名,3-旧地址合并)',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7028 DEFAULT CHARSET=utf8 COMMENT='地区(市/区/县/州)';
+) ENGINE=InnoDB AUTO_INCREMENT=54214 DEFAULT CHARSET=utf8 COMMENT='地区(市/区/县/州)';
